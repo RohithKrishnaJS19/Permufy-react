1b7b6a790f492459d497801f028241b42eb7f5bd
RohithKrishnaJS19 <rohithkrishnajs1908@gmail.com>
2026-03-26 15:51:47 +0530

    Perfumy Project using React

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import {App} from "./App.js"
+
+const root = ReactDOM.createRoot(document.getElementById("container"))
+
+root.render(<App></App>)
